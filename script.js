a61360ba68c48701dd189afe2c554da84383b266
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-06 11:08:15 +0200

    1

---
+++ script.js
@@ -0,0 +1,14 @@
+let array = ['455', 
+            87.15, 
+            true,
+            undefined, 
+            null, 
+            'false', 
+            [], 
+            {}
+        ];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(typeof array[i], array[i]+6);
+}
+
