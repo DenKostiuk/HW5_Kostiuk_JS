9ef1c1ee2c51ee4a5038cfd0afb453d963140691
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-09 16:21:56 +0200

    2

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-let array = ['455', 
-            87.15, 
-            true,
-            undefined, 
-            null, 
-            'false', 
-            [], 
-            {}
-        ];
+let users = [
+    {
+        name: "John",
+        surname: "Jonson",
+        age: 20,
+        salary: 1000,
+    },
+    {
+        name: "Mark",
+        surname: "Markov",
+        age: 25,
+        salary: 1500,
+    },
+    {
+        name: "Ben",
+        surname: "Banan",
+        age: 16,
+        salary: 0,
+    },
+    {
+        name: "Rob",
+        surname: "Cobman",
+        age: 17,
+        salary: 0,
+    },
+    {
+        name: "Evan",
+        surname: "Voland",
+        age: 32,
+        salary: 2500,
+    },
+];
 
-for (let i = 0; i < array.length; i++) {
-    console.log(typeof array[i], array[i]+6);
-}
+let midSalary = 0;
+
+for (let i = 0; i < users.length; i++) {
+    midSalary += users[i].salary / users.length;
+    if (users[i].age < 18) {
+        users[i].willDrink = false;
+        console.log(users[i].name, users[i].age);
+        console.log(`Удален пользователь ` +users[i].name,`ему было ` +users[i].age);
+        delete users[i];
+        users[i] = null;
+    } else {
+        users[i].willDrink = true;
+    }
+};
+
+console.log(`Средняя зарплата пользователей ` + midSalary);
+
+console.log(users);
+
 
